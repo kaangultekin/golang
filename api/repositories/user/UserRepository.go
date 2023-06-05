b569f3e8722384bac8d6411b69ea67f96fa05bff
kaangultekin <kaangultekin1907@gmail.com>
2023-06-05 12:57:54 +0300

    edit

--- api/repositories/user/UserRepository.go
+++ api/repositories/user/UserRepository.go
@@ -12,7 +12,7 @@ type UserRepository struct{}
 func (ur *UserRepository) GetByEmail(email string) (*userModels.User, error) {
 	var user userModels.User
 
-	result := config.DB.Where("email = ?", email).First(&user)
+	result := config.DB.Where("email", email).First(&user)
 
 	if result.Error != nil {
 		return nil, errors.New(messageConstants.ErrUserNotFound)
@@ -24,7 +24,9 @@ func (ur *UserRepository) GetByEmail(email string) (*userModels.User, error) {
 func (ur *UserRepository) GetById(id int) (*userModels.User, error) {
 	var user userModels.User
 
-	result := config.DB.Where("id = ? AND status = ?", id, 1).First(&user)
+	result := config.DB.Where("id", id).
+		Where("status", 1).
+		First(&user)
 
 	if result.Error != nil {
 		return nil, errors.New(messageConstants.ErrUserNotFound)
